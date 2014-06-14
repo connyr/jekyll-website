347da5d9f82651cceba956798c15a7cc07052b46
Cornelia Rehbein <cornelia.reh@gmail.com>
2014-06-14 15:59:53 +0200

    cleaning up merge remains and styling blogwrap content

--- custom/custom.js
+++ custom/custom.js
@@ -16,7 +16,8 @@ jQuery( window ).load( function() {
     container.masonry({
         gutter: gutter,
         itemSelector: '.post',
-        columnWidth: '.post'
+        columnWidth: '.post',
+        
     });
     
     
@@ -24,37 +25,37 @@ jQuery( window ).load( function() {
     // This code fires every time a user resizes the screen and only affects .post elements
     // whose parent class is .container-fluid. Triggers resize so nothing looks weird.
     
-    jQuery( window ).bind( 'resize', function(){
-        if ( jQuery( '#posts' ).parent().hasClass( 'container-fluid' ) ) {
+    // jQuery( window ).bind( 'resize', function(){
+    //     if ( jQuery( '#posts' ).parent().hasClass( 'container-fluid' ) ) {
             
             
             
-            // Resets all widths to 'auto' to sterilize calculations
+    //         // Resets all widths to 'auto' to sterilize calculations
             
-            post_width = jQuery( '.post' ).width() + gutter;
-            jQuery( '.container-fluid #posts, body > .container-fluid' ).css( 'width', 'auto');
-            
-            
-            
-            // Calculates how many .post elements will actually fit per row. Could this code be cleaner?
-            
-            posts_per_row = jQuery( '#posts' ).innerWidth() / post_width;
-            floor_posts_width = ( Math.floor( posts_per_row ) * post_width ) - gutter;
-            ceil_posts_width = ( Math.ceil( posts_per_row ) * post_width ) - gutter;
-            posts_width = ( ceil_posts_width > jQuery( '#posts' ).innerWidth() ) ? floor_posts_width : ceil_posts_width;
-            if ( posts_width == jQuery( '.post' ).width() ) posts_width = '100%';
+    //         post_width = jQuery( '.post' ).width() + gutter;
+    //         jQuery( '.container-fluid #posts, body > .container-fluid' ).css( 'width', 'auto');
             
             
             
-            // Ensures that all top-level .container-fluid elements have equal width and stay centered
+    //         // Calculates how many .post elements will actually fit per row. Could this code be cleaner?
             
-            jQuery( '.container-fluid #posts, body > .container-fluid' ).css( 'width', posts_width );
-            jQuery( 'body > .container-fluid' ).css({ 'margin': '0 auto' });
+    //         posts_per_row = jQuery( '#posts' ).innerWidth() / post_width;
+    //         floor_posts_width = ( Math.floor( posts_per_row ) * post_width ) - gutter;
+    //         ceil_posts_width = ( Math.ceil( posts_per_row ) * post_width ) - gutter;
+    //         posts_width = ( ceil_posts_width > jQuery( '#posts' ).innerWidth() ) ? floor_posts_width : ceil_posts_width;
+    //         if ( posts_width == jQuery( '.post' ).width() ) posts_width = '100%';
+            
+            
+            
+    //         // Ensures that all top-level .container-fluid elements have equal width and stay centered
+            
+    //         jQuery( '.container-fluid #posts, body > .container-fluid' ).css( 'width', posts_width );
+    //         jQuery( 'body > .container-fluid' ).css({ 'margin': '0 auto' });
         
         
         
-        }
-    }).trigger( 'resize' );
+    //     }
+    // }).trigger( 'resize' );
     
  
  
