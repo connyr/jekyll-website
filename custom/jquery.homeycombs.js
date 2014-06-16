a2f879db8ccb141646c439d72539c41318df0ca4
Cornelia Rehbein <cornelia.reh@gmail.com>
2014-06-16 22:18:05 +0200

    cleaned test files and set styling for content sections on the home index

--- custom/jquery.homeycombs.js
+++ custom/jquery.homeycombs.js
@@ -4,7 +4,7 @@
 
         // Establish our default settings
         var settings = $.extend({
-            combWidth: 250,
+            combWidth: 350,
             margin: 10
         }, options);
 
